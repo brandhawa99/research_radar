8c065a80d9d9678f54958de8da6cf3c0be20a555
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-05 00:19:27 -0800

    red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <main className=" flex min-h-screen flex-col items-center justify-between p-24">
       <div className="max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <header className="border-solid border-2 border-black p-4 rounded-md">
-          <h1 className="text-[color:red] text-xl">
+        <header className="bg-red-200 border-solid border-2 border-black p-4 rounded-md">
+          <h1 className="text-[color:slate] text-xl">
             Research Radar
           </h1>
         </header>
