e16465326555fecf1881e8b457e258c104f2efe5
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-05 23:42:56 -0800

    created testimonials element

---
+++ src/components/ui/testimonalcard.tsx
@@ -0,0 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card"
+
+export default function Testimonalcard(props:any) {
+  
+  return (
+
+  <>
+  <Card>
+  <CardHeader>
+    <CardTitle>Layla Al Awssy</CardTitle>
+  </CardHeader>
+  <CardContent>
+    <p>
+      Reserach radar changed my life
+    </p>
+  </CardContent>
+</Card>
+
+  </>
+  )
+}
