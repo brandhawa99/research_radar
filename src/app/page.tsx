c8afeb46bcfbe276624106e6362e5ab2aba33cc9
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-05 23:43:13 -0800

    added testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { DiscIcon } from "@radix-ui/react-icons"
 import Link from "next/link";
-
+import {buttonVariants} from '@/components/ui/button'
+import Testimonalcard from "@/components/ui/testimonalcard";
 export default function Home() {
   return (
     <main className=" flex w-100 flex-col items-center bg-zinc-100">
@@ -12,17 +13,47 @@ export default function Home() {
           </h1>
         </header>
       </div>
-      <div className="flex flex-col justify-center items-center py-8 px-10 gap-7">
-        <h1 className="text-center text-5xl font-extrabold text-balance bg-gradient-to-r from-red-700 via-red-500 to-red-900 text-transparent bg-clip-text">
+      <div className="flex flex-col py-8 gap-2">
+        <h1 className="flex flex-col text-center text-5xl font-extrabold text-balance bg-gradient-to-r from-red-700 via-red-500 to-red-900 text-transparent bg-clip-text">
+        <a className="text-black  px-125-600red-600 text-sm py-5">Research Opportunity Search Tool</a>
             Simplify Your Research Job Search with Research Radar. 
         </h1>
-        <p className="text-md text-center px-5 text-balance">
+        <p className="text-md text-center px-10 py-5 text-balance">
           Are you tired of endless job searches, scattered across multiple platforms, and wasting precious time filtering through irrelevant listings? Say goodbye to the hassle and hello to Research Radar - your all-in-one destination for finding the perfect research job.
         </p>
-        <Link href={"/careers"}>
-            Explore Opportunities
-        </Link>
+        <div className="flex gap-5 flex-row self-center  items-center">
+            <Link href={"/careers"} className={buttonVariants({})}>
+              Explore Opportunities
+            </Link>
+            <Link href={"/signup"} className={buttonVariants({variant: "outline"})}>
+              Sign Up
+            </Link>
+        </div>
+
+        <h2 className="text-[color:black] font-2xl self-center py-10">
+          Testimonials
+        </h2>
+        <div className="flex overflow-hidden space-x-16">
+   
+          <div className="flex space-x-16 animate-loop-scroll">
+            <Testimonalcard />
+            <Testimonalcard />
+            <Testimonalcard />
+           </div>
+           <div className="flex space-x-16 animate-loop-scroll">
+
+            <Testimonalcard />
+            <Testimonalcard />
+            <Testimonalcard />
+          </div>
+        </div>
+
+        
       </div>
+
+      <footer className="bg-zinc-200 w-screen p-10">
+        footer
+      </footer>
 
     </main>
 )}
