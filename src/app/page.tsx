cff91107387ceccfdaae3b99d9b21396f91138ca
brandhawa99 <baltej.randhawa0@gmail.com>
2024-03-07 21:37:33 -0800

    refactored landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <main className=" flex flex-col items-center bg-zinc-100">
       <div>
-        <Iconheader/>
+        <Iconheader />
       </div>
       <div className="flex flex-col py-2 gap-2">
         <section>
           <h1 className="flex flex-col text-center text-5xl font-extrabold text-balance bg-gradient-to-r from-red-700 via-red-500 to-red-900 text-transparent bg-clip-text">
-            <a className="text-black  px-125-600red-600 text-sm py-5">
+            <p className="text-black  px-125-600red-600 text-sm py-5">
               Research Opportunity Search Tool
-            </a>
+            </p>
             Simplify Your Research Job Search with Research Radar.
           </h1>
           <p className="text-md text-center px-10 py-5 text-balance">
@@ -39,8 +39,8 @@ export default function Home() {
           className="self-center p-10 rounded-md"
           src={landingzoneImg}
           alt="test tubes"
-          loading="lazy"
           width={600}
+          priority={true}
           style={{ borderRadius: "50px" }}
         />
       </div>
